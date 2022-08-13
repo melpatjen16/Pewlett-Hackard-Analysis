a2886158bfde8edff37007e14a860c440abddd3c
melpatjen16 <mp.jenkins16@gmail.com>
2022-08-12 21:52:24 -0400

    Update data tables and code

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -210,3 +210,16 @@ ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no)
 WHERE d.dept_no = ('d007');
+
+--Create list of retirees in Development and Sales
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO sales_retire_devsal
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_no IN ('d005','d007');
