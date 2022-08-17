49918670ad35a90902048e93172af4b9b4520eb9
melpatjen16 <mp.jenkins16@gmail.com>
2022-08-16 20:34:01 -0400

    Employee Challange additions

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -15,12 +15,42 @@ ORDER BY e.emp_no;
 SELECT * FROM employees_retire;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) _____,
-______,
-______,
-______
+-- Select current employees and titles
+SELECT DISTINCT ON (er.emp_no) er.emp_no,
+er.first_name,
+er.last_name,
+er.title
+INTO unique_titles
+FROM employees_retire AS er
+WHERE er.to_date = '9999-01-01'
+ORDER BY er.emp_no, er.to_date DESC;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+SELECT * FROM unique_titles;
+
+SELECT COUNT(DISTINCT u.emp_no) AS "count", u.title
+INTO retiring_titles
+FROM unique_titles AS u
+GROUP BY u.title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+
+--Create table employees information
+-- Mentorship elibility
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date, 
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')	
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
